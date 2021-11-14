54fb6e7472fd912540ccfb5e2764da8d5707f4b2
Wooseong Yang/양우성 <83999003+YangWooseong-vuno@users.noreply.github.com>
2021-11-14 18:15:56 +0900

    react-query: data transform callback

--- React/react-query/react-query.md
+++ React/react-query/react-query.md
@@ -1,3 +1,5 @@
 react-query 기능
 
 1. cache: default 5 분 동안 fetch한 데이터를 캐싱한다. useQuery에 인자로 넘겨준 키값으로 조회해서 캐시가 있으면 그걸 일단 보여준다음에, 백그라운드에서 다시 fetching을 해서 새 데이터를 보여준다. 데이터 업데이트가 없었다면터동일 데이터를, 있었다면 새로운 데이터를 보여준다. 캐싱된 데이터가 있다면 isLoading = false, isFetching = true -> false 가 되는 것을 확인할 수 있다. 만약 데이터가 자주 업데이트 되지 않아서 필요없는 fetching 을 막고싶다면 stale time (default 0sec) 을 설정하면 된다.
+
+2. onSuccess, onError: data fetch 성공 실패 이후에 callback 함수 지정 가능.. side effect로 활용가능 할 것 같다. login, admin 에 의한 유저 정보 편집, 환자 상태 업데이트 이후에 나오는 modal을 여기서 설정하면 편할 것 같다.
--- React/react-query/src/components/RQSuperHeroes.page.js
+++ React/react-query/src/components/RQSuperHeroes.page.js
@@ -7,28 +7,15 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
-  const onSuccess = (data) => {
-    console.log('Perform side effect after data fetching', data)
-    console.log(count + 1)
-    setCount(count + 1)
-    count + 1 > 2 && setRefetch(false)
+  const dataTransform = (data) => {
+    return data.data.map((hero) => hero.name)
   }
-
-  const onError = (error) => {
-    console.log('Perform side effect after encountering error', error)
-    setRefetch(false)
-  }
-
-  const [refetch, setRefetch] = useState(true)
-  const [count, setCount] = useState(0)
 
   const { isLoading, data, isError, error, isFetching } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
-      onSuccess,
-      onError,
-      refetchInterval: refetch ? 2000 : 0,
+      select: dataTransform,
     },
   )
   if (isLoading) {
@@ -37,14 +24,13 @@ export const RQSuperHeroesPage = () => {
   if (isError) {
     return <h2>{error.message}</h2>
   }
-
   console.log(data)
 
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
-      {data?.data.map((hero, idx) => {
-        return <div key={idx}>{hero.name}</div>
+      {data.map((name, idx) => {
+        return <div key={name}>{name}</div>
       })}
     </>
   )
